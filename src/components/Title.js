b7b1232691c907035d90edd97834cdcc52e1d489
yusoffnike01 <ronaldoyusoff@gmail.com>
2022-02-06 00:50:35 +0800

    update word

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ const Title =()=>{
   return(<div className="title">
 <h1>My Album</h1>
 <h2>Your Pictures</h2>
-<p>blaasasasasas</p>
+<p>Welcome Our Public Pictures</p>
   </div>)
 }
 export default Title; 
